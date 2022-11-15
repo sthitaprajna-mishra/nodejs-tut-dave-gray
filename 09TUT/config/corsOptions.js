78b3e15b04a6aad4fdaff89afca0667618dfe54c
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2022-11-15 20:13:00 +0530

    09 TUT finished

---
+++ 09TUT/config/corsOptions.js
@@ -0,0 +1,19 @@
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.google.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log(`Origin passed in: ${origin}`);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
